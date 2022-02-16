a988e78a4d1d25c0529995c1e5aaf56dded9f592
Sh0w3D <49204446+Sh0w3D@users.noreply.github.com>
2022-02-16 19:02:34 +0100

    Added new tab in shared layout, new theme and Category Controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using dotNETBlog.Data;
+using dotNETBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNETBlog.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly applicationDbContext _db;
+        public CategoryController(applicationDbContext db) { _db = db; }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
