d6d9a8758b99d33fba52b0d845d3f76ce54a338d
Sh0w3D <49204446+Sh0w3D@users.noreply.github.com>
2022-03-01 17:37:48 +0100

    Added Category(Controller): Edit and Delete options that work with database

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -13,8 +13,6 @@ namespace dotNETBlog.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
-
-        [HttpGet]
         public IActionResult Create()
         {
             return View();
@@ -29,7 +27,8 @@ namespace dotNETBlog.Controllers
             {
                 ModelState.AddModelError("name", "The Display Order cannot exactly match the name");
             }
-            if (ModelState.IsValid) {
+            if (ModelState.IsValid)
+            {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
@@ -37,6 +36,69 @@ namespace dotNETBlog.Controllers
 
             return View(obj);
 
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0) return NotFound();
+
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if(categoryFromDb == null) return NotFound();
+
+            return View(categoryFromDb);
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+
+            if (obj == null) return NotFound();
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully!";
+            return RedirectToAction("Index");
         }
     }
 }
