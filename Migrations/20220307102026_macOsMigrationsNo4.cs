a407befe1acc6bf6d5f9ad687104341958437d99
Kamil Oberaj <kamil.oberaj@gmail.com>
2022-03-07 19:50:30 +0100

    Add new migrations, fix requirements problem and add user management functionality

---
+++ Migrations/20220307102026_macOsMigrationsNo4.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotNETBlog.Migrations
+{
+    public partial class macOsMigrationsNo4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nick",
+                table: "Categories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nick",
+                table: "Categories",
+                type: "varchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
